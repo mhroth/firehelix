806499499895bf02ef7a8954cf7c56454e53450b
Martin Roth <mhroth@gmail.com>
2015-06-28 00:19:30 +0100

    update

--- src/firehelix.c
+++ src/firehelix.c
@@ -35,8 +35,10 @@
 
 #define INP_GPIO(g) *(gpio.addr + ((g)/10)) &= ~(7<<(((g)%10)*3))
 #define OUT_GPIO(g) *(gpio.addr + ((g)/10)) |=  (1<<(((g)%10)*3))
-#define GPIO_SET *(gpio.addr + 7) // sets bits which are 1 ignores bits which are 0
-#define GPIO_CLR *(gpio.addr + 10) // clears bits which are 1 ignores bits which are 0
+// sets bits which are 1 ignores bits which are 0
+#define GPIO_SET(g) *(gpio.addr + 7) = (1 << g)
+// clears bits which are 1 ignores bits which are 0
+#define GPIO_CLR(g) *(gpio.addr + 10) = (1 << g)
 #define GPIO_READ(g) *(gpio.addr + 13) &= (1<<(g))
 
 // configure pins for output
@@ -64,29 +66,29 @@ struct bcm2835_peripheral gpio = {GPIO_BASE};
 // Some forward declarations...
 // Exposes the physical address defined in the passed structure using mmap on /dev/mem
 static int map_peripheral(struct bcm2835_peripheral *p) {
-   // Open /dev/mem
-   if ((p->mem_fd = open("/dev/mem", O_RDWR|O_SYNC) ) < 0) {
-      printf("Failed to open /dev/mem, try checking permissions.\n");
-      return -1;
-   }
+  // Open /dev/mem
+  if ((p->mem_fd = open("/dev/mem", O_RDWR|O_SYNC) ) < 0) {
+    printf("Failed to open /dev/mem, try checking permissions.\n");
+    return -1;
+  }
 
-   p->map = mmap(
-      NULL,
-      MMAP_BLOCK_SIZE,
-      PROT_READ|PROT_WRITE,
-      MAP_SHARED,
-      p->mem_fd, // File descriptor to physical memory virtual file '/dev/mem'
-      p->addr_p // Address in physical map that we want this memory block to expose
-   );
+  p->map = mmap(
+    NULL,
+    MMAP_BLOCK_SIZE,
+    PROT_READ|PROT_WRITE,
+    MAP_SHARED,
+    p->mem_fd, // File descriptor to physical memory virtual file '/dev/mem'
+    p->addr_p // Address in physical map that we want this memory block to expose
+  );
 
-   if (p->map == MAP_FAILED) {
-      perror("mmap");
-      return -1;
-   }
+  if (p->map == MAP_FAILED) {
+    perror("mmap");
+    return -1;
+  }
 
-   p->addr = (volatile unsigned int *) p->map;
+  p->addr = (volatile unsigned int *) p->map;
 
-   return 0;
+  return 0;
 }
 
 static void unmap_peripheral(struct bcm2835_peripheral *p) {
@@ -111,8 +113,8 @@ static void hv_sendHook(double timestamp, const char *receiverName,
   if (receiverName[0] == '#') { // minimise overhead of sendhook for
     if (!strncmp(receiverName, "#PIN_00", 7)) {
       // TODO(mhroth): do this correctly
-      if (hv_msg_getFloat(m, 0) == 0.0f) GPIO_SET = 1 << PIN_07;
-      else GPIO_CLR = 1 << PIN_07;
+      if (hv_msg_getFloat(m, 0) == 0.0f) GPIO_CLR(PIN_07);
+      else GPIO_SET(PIN_07);
     } else if (!strncmp(receiverName, "#PIN_01", 7)) {
       // TODO(mhroth): etc.
     }
@@ -162,7 +164,7 @@ int main(int argc, char *argv[]) {
   }
 
   // TODO(mhroth): set all pins to 0 and unmap everything
-  GPIO_CLR = 1 << PIN_07;
+  GPIO_CLR(PIN_07);
   unmap_peripheral(&gpio);
 
   // free heavy
